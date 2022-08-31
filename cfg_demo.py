d52fd632643485d2b717b18057936c226b33b887
gislite <gislite@live.cn>
2022-08-31 14:59:01 +0800

    add cfg* file.

---
+++ cfg_demo.py
@@ -0,0 +1,6 @@
+machines = {
+    'aliyun6': {'u': 'gis', 'h': '39.107.109.xx', 'p': ''},
+    'aliyun3': {'u': 'gis', 'h': '121.42.29.xx', 'p': ''},
+}
+
+mach = machines['aliyun3']
---
+++ cfg_server.py
@@ -0,0 +1,18 @@
+'''
+config file on server side.
+'''
+
+machines = {
+    'aliyun6': {'u': 'gis', 'h': '39.107.109.xx', 'p': ''},
+}
+
+mach = machines['aliyun6']
+
+######################################################################################
+
+IP = 'webgis.pub'
+
+SITE_URL = '//webgis.pub'
+
+TILE_SVR = '//www.gislite.com:8888'
+GIS_BASE = '/home/gis/ws/geodata2'
