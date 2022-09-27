ed5f6885dd5b1ca3ff284baff1519ee1d96f92eb
gislite <gislite@live.com>
2022-09-27 09:40:10 +0800

    clean the codes.

--- config.py
+++ config.py
@@ -3,7 +3,10 @@
 # IP = 'webgis.pub'
 
 # SITE_URL = '//webgis.pub'
-SITE_URL = '//webgis.pub'
+try:
+    from cfg import SITE_URL
+except:
+    SITE_URL = '//webgis.pub'
 
 TILE_SVR = '//www.gislite.com:8888'
 GIS_BASE = '/home/bk/ws/geodata2'
