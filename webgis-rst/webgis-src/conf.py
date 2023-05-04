48e30f8dd94b933718797a995ddbb322a4fa4dbc
gislite <gislite@live.cn>
2023-05-04 22:01:07 +0800

    customize for style.

--- webgis-rst/webgis-src/conf.py
+++ webgis-rst/webgis-src/conf.py
@@ -23,3 +23,20 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": "https://github.com/gislite/webgis-pub",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-square-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+   ]
+}
+
+html_logo = "_static/webgis-logo.png"
