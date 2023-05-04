e9bdccd53220208102aeb7225126b01aef1d790a
gislite <gislite@live.cn>
2023-05-04 21:13:15 +0800

    just commit.

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-python3 build_mapfile.py
-/xpy/bin/python3 build_site.py
+# python3 r1_gen_mapfile.py
+# python3 r2_place_inc.py
+# python3 r3_pub_rst.py
+cd _pub
+. /xpy/bin/activate && make html
+sudo rsync -avp _build/html/ /owg/
+sudo chown -R www-data.www-data /owg/
