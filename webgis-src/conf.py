163f9ea2b5583b6ccb8c06f2e270d49d1144d948
gislite <gislite@live.cn>
2023-07-14 08:30:18 +0800

    just commit.

--- webgis-src/conf.py
+++ webgis-src/conf.py
@@ -1,45 +1,28 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Open Source WebGIS'
 copyright = 'Since 2020'
 author = 'gislite'
-release = 'v 0.13'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = 'v 0.14'
 
 extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 
 html_theme_options = {
     "icon_links": [
         {
-            # Label for this link
             "name": "GitHub",
-            # URL where the link will redirect
             "url": "https://github.com/gislite/webgis-pub",  # required
-            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
             "icon": "fa-brands fa-square-github",
-            # The type of image to be used (see below for details)
             "type": "fontawesome",
         }
-   ]
+    ]
 }
 
 html_logo = "_static/webgis-logo.png"
 
-locale_dirs = ['locales']   # path is example but recommended.
-gettext_compact = False     # optional.
+locale_dirs = ['locales']
+gettext_compact = True
