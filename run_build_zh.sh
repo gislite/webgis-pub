2332103bb4d6520276cddfb812f11e84b2f094bf
gislite <gislite@live.cn>
2023-07-14 08:36:14 +0800

    just commit.

--- run_build_zh.sh
+++ run_build_zh.sh
@@ -1,6 +1,6 @@
 
 make gettext
 
-sphinx-intl update -p _build/gettext -d pyqgis-src/locales -l zh
+sphinx-intl update -p _build/gettext -d webgis-src/locales -l zh
 
 make -e SPHINXOPTS="-Dlanguage='zh'" html
