36cceb0c7b4598dda514a67556d5665a90e14400
Ybiao <478747656@qq.com>
2024-04-12 14:28:53 +0800

    xiugai

--- QuaGIS/src/pages/mapbox_index/mapbox_foo.vue
+++ QuaGIS/src/pages/mapbox_index/mapbox_foo.vue
@@ -51,7 +51,7 @@ export default {
         bearing: 0, // 地图的初始方向，值是北的逆时针度数，默认是0，即是正北
         antialias: true, // 抗锯齿，通过false关闭提升性能
         minZoom: 3,
-        maxZoom: 18,
+        // maxZoom: 18,
         // center: [54.84472718131394, 33.89394107223532], // 初始坐标系，这个是南京建邺附近
         // zoom: 2.267,     // starting zoom 地图初始的拉伸比例
         // pitch: 60,  //地图的角度，不写默认是0，取值是0-60度，一般在3D中使用
