0f0577d439586bf21f8ba349685ddd7ed004c0aa
bukun <bukun@osgeo.cn>
2024-07-26 10:23:11 +0800

    mark.

--- QuaGIS/run_pod.sh
+++ QuaGIS/run_pod.sh
@@ -1,8 +1,8 @@
 git pull
 
-podman run --rm -it -v `pwd`:/ws node:lts-alpine sh -c 'cd /ws && yarn'
-podman run --rm -it -v `pwd`:/ws node:lts-alpine sh -c 'cd /ws && yarn quasar build'
-# podman run --rm -it -p 6792:9000 -v `pwd`:/ws node:lts-alpine sh -c 'cd /ws && yarn quasar dev'
+podman run --rm -it -v `pwd`:/ws node:18-alpine sh -c 'cd /ws && yarn'
+podman run --rm -it -v `pwd`:/ws node:18-alpine sh -c 'cd /ws && yarn quasar build'
+# podman run --rm -it -p 6792:9000 -v `pwd`:/ws node:18-alpine sh -c 'cd /ws && yarn quasar dev'
 
 
 cd dist/spa/ && python3 -m http.server 6795
